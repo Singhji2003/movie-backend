3c5ed50a2ef47b8aba6e6ebee37ddb9c8dedbc30
Arpan <arpansinghrajput123@gmail.com>
2025-11-10 15:32:02 +0530

    udpated the routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import moviePosterRoutes from "./routes/moviePosterRoutes.js";
+import tvSeriesRoutes from "./routes/tvSeriesRoutes.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
+app.use("/api/tv-series", tvSeriesRoutes);
 app.use("/api/movie-posters", moviePosterRoutes);
 app.use((err, req, res, next) => {
   console.error("Global Error:", err);
